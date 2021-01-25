4ea5c25dd118aad019d2865fea4bd39a26b84d4b
Daniel Barlow <dan@telent.net>
2021-01-25 21:07:19 +0000

    make test fns void as they don't return anything

--- tests.c
+++ tests.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-static int test_xy_to_index()
+static void test_xy_to_index()
 {
   assert(xy_to_index(0,0)==0);
   assert(xy_to_index(8,0)==8);
@@ -16,13 +16,13 @@ static int test_xy_to_index()
   assert(xy_to_index(2,2)==32);
 }
 
-static int test_set_node_id()
+static void test_set_node_id()
 {
   assert(! strcmp("0123456789ab", set_node_id("01:23:45:67:89:ab")));
   assert(! strcmp("0123456789ab", node_id));
 }
 
-static int test_make_topic()
+static void test_make_topic()
 {
   char topic[sizeof sizeof(MQTT_TOPIC_PREFIX) + sizeof(node_id) + 11];
   puts(make_topic(topic, sizeof topic, "/mytopic"));
